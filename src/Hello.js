a7de1c25c1d73020b5747d2686bd449594976473
shaheer shakir <shaheershakir22@gmail.com>
2023-05-08 12:14:55 +0530

    added props as object

--- src/Hello.js
+++ src/Hello.js
@@ -4,9 +4,9 @@ import './hello.css'
 class Hello extends Component {
     render() {
         return (
-        <div>
-            <h1>Hello World</h1>
-            <p>This is a paragraph</p>
+        <div className='f1 tc'>
+            <h1>Hello World</h1> 
+            <p>{this.props.greeting}</p>
         </div> 
         );
     }
